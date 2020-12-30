83dcda0988a28e1dfd3320eda607fcbeff6475e5
brust <brust.cn@gmail.com>
2020-12-30 16:03:38 +0800

    火币行情

---
+++ src/test/java/cn/brust/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.brust.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
